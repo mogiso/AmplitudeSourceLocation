cd799af164aa398ea551e41c170377710a7b7590
Masashi Ogiso <mogiso@mri-jma.go.jp>
2020-06-02 14:21:47 +0900

    fix typo

--- set_velocity_model.F90
+++ set_velocity_model.F90
@@ -10,7 +10,7 @@ module set_velocity_model
     real(kind = fp), intent(in)  :: z_min, dz                  !!size of depth direction
     real(kind = fp), intent(out) :: velocity(:, :, :), qinv(:, :, :)
 
-    real(kind = fp), paramter    :: VpVs = sqrt(3.0_fp)
+    real(kind = fp), parameter   :: VpVs = sqrt(3.0_fp)
     integer :: i, nlon, nlat, nz
     real(kind = fp) :: depth
 
@@ -24,15 +24,15 @@ module set_velocity_model
       if(i .eq. 1) write(0, '(a)') "velocity/Qinv = meakan 1d structure"
       if(depth .lt. 0.5_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (3.2_fp - 3.0_fp) / (0.5_fp - 0.0_fp) * depth + 3.0_fp
-      elseif(depth .ge. 0.5_fp .and depth .lt. 1.0_fp) then
+      elseif(depth .ge. 0.5_fp .and. depth .lt. 1.0_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (4.5_fp - 3.2_fp) / (1.0_fp - 0.5_fp) * (depth - 0.5_fp) + 3.2_fp
-      elseif(depth .ge. 1.0_fp .and depth .lt. 1.5_fp) then
+      elseif(depth .ge. 1.0_fp .and. depth .lt. 1.5_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (5.0_fp - 4.5_fp) / (1.5_fp - 1.0_fp) * (depth - 1.0_fp) + 4.5_fp
-      elseif(depth .ge. 1.5_fp .and depth .lt. 2.0_fp) then
+      elseif(depth .ge. 1.5_fp .and. depth .lt. 2.0_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (5.133_fp - 5.0_fp) / (2.0_fp - 1.5_fp) * (depth - 1.5_fp) + 5.0_fp
-      elseif(depth .ge. 2.0_fp .and depth .lt. 2.5_fp) then
+      elseif(depth .ge. 2.0_fp .and. depth .lt. 2.5_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (5.262_fp - 5.133_fp) / (2.5_fp - 2.0_fp) * (depth - 2.0_fp) + 5.133_fp
-      elseif(depth .ge. 2.5_fp .and depth .lt. 3.0_fp) then
+      elseif(depth .ge. 2.5_fp .and. depth .lt. 3.0_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (5.383_fp - 5.262_fp) / (3.0_fp - 2.5_fp) * (depth - 2.5_fp) + 5.262_fp
       elseif(depth .ge. 3.0_fp) then
         velocity(1 : nlon, 1 : nlat, i) = (5.494_fp - 5.383_fp) / (3.5_fp - 3.0_fp) * (depth - 3.0_fp) + 5.383_fp
