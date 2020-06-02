bfbb514a77ec7a7b23afb14d03de271689fa4de2
Masashi Ogiso <mogiso@mri-jma.go.jp>
2020-06-02 14:12:25 +0900

    add 1d structure for meakandake volcano

--- set_velocity_model.F90
+++ set_velocity_model.F90
@@ -10,6 +10,7 @@ module set_velocity_model
     real(kind = fp), intent(in)  :: z_min, dz                  !!size of depth direction
     real(kind = fp), intent(out) :: velocity(:, :, :), qinv(:, :, :)
 
+    real(kind = fp), paramter    :: VpVs = sqrt(3.0_fp)
     integer :: i, nlon, nlat, nz
     real(kind = fp) :: depth
 
@@ -19,29 +20,39 @@ module set_velocity_model
   
     do i = 1, nz
       depth = z_min + dz * real(i - 1, kind = fp)
-#ifdef VEL_1D
-      if(i .eq. 0) write(0, '(a)') "velocity = 1d structure"
-      if(depth .lt. 0.0_fp) then
-        velocity(1 : nlon, 1 : nlat, i) = (3.0_fp - 1.5_fp) / (0.0_fp + 1.5_fp) * (depth + 1.5_fp) + 1.5_fp
+#ifdef MEA_1D
+      if(i .eq. 1) write(0, '(a)') "velocity/Qinv = meakan 1d structure"
+      if(depth .lt. 0.5_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (3.2_fp - 3.0_fp) / (0.5_fp - 0.0_fp) * depth + 3.0_fp
+      elseif(depth .ge. 0.5_fp .and depth .lt. 1.0_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (4.5_fp - 3.2_fp) / (1.0_fp - 0.5_fp) * (depth - 0.5_fp) + 3.2_fp
+      elseif(depth .ge. 1.0_fp .and depth .lt. 1.5_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (5.0_fp - 4.5_fp) / (1.5_fp - 1.0_fp) * (depth - 1.0_fp) + 4.5_fp
+      elseif(depth .ge. 1.5_fp .and depth .lt. 2.0_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (5.133_fp - 5.0_fp) / (2.0_fp - 1.5_fp) * (depth - 1.5_fp) + 5.0_fp
+      elseif(depth .ge. 2.0_fp .and depth .lt. 2.5_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (5.262_fp - 5.133_fp) / (2.5_fp - 2.0_fp) * (depth - 2.0_fp) + 5.133_fp
+      elseif(depth .ge. 2.5_fp .and depth .lt. 3.0_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (5.383_fp - 5.262_fp) / (3.0_fp - 2.5_fp) * (depth - 2.5_fp) + 5.262_fp
+      elseif(depth .ge. 3.0_fp) then
+        velocity(1 : nlon, 1 : nlat, i) = (5.494_fp - 5.383_fp) / (3.5_fp - 3.0_fp) * (depth - 3.0_fp) + 5.383_fp
+      endif
+
+      !!the values of Qinv are taken from Kumagai et al (2019): case of Nevado
+      !del Ruiz volcano (layer thickness is different)
+      if(depth .lt. 1.0_fp) then
+        qinv(1 : nlon, 1 : nlat, i) = 1.0_fp / 40.0_fp
       else
-        velocity(1 : nlon, 1 : nlat, i) = 3.0_fp
+        qinv(1 : nlon, 1 : nlat, i) = 1.0_fp / 180.0_fp
       endif
 #else
-      if(i .eq. 0) write(0, '(a)') "velocity = CONST"
+      if(i .eq. 1) write(0, '(a)') "Velocity/Qinv = constant"
       velocity(1 : nlon, 1 : nlat, i) = 2.5_fp
+      qinv(1 : nlon, 1 : nlat, i) = 1.0_fp / 50.0_fp
 #endif
 
-#ifdef QINV_1D
-      if(i .eq. 0) write(0, '(a)') "qinv = 1d_structure"
-      if(depth .lt. 0.0_fp) then
-        qinv(1 : nlon, 1 : nlat, i) = 0.05_fp
-      else
-        qinv(1 : nlon, 1 : nlat, i) = 0.01_fp
-      endif
-#else
-      if(i .eq. 0) write(0, '(a)') "qinv = CONST"
-      qinv(1 : nlon, 1 : nlat, i) = 0.02_fp
-#endif
+      !!Vp to Vs
+      velocity(1 : nlon, 1 : nlat, i) = velocity(1 : nlon, 1 : nlat, i) / VpVs
 
     enddo
     return
