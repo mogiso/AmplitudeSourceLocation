bfdfd99bff6bd3dcf61caf9f12a0771ded6600b9
Masashi Ogiso <mogiso@mri-jma.go.jp>
2021-01-19 19:48:46 +0900

    add error stop when cannot read waveform from stdin

--- read_shmdump.F90
+++ read_shmdump.F90
@@ -21,12 +21,16 @@ module read_shmdump
     integer,                parameter :: nsample_int_max = 205
     character(len = 4)                :: chid_tmp
     integer                           :: i, j, waveform_int(nsample_int_max), sample_int, nsta, nbuf_sec, ndata_tmp, &
-    &                                    yr_tmp, mo_tmp, dy_tmp, hh_tmp, mm_tmp, ss_tmp, nch
+    &                                    yr_tmp, mo_tmp, dy_tmp, hh_tmp, mm_tmp, ss_tmp, nch, ios
 
     nsta = ubound(chid_order, 1)
     nbuf_sec = ubound(ndata_sec, 1)
 
-    read(6, *) yr_tmp, mo_tmp, dy_tmp, hh_tmp, mm_tmp, ss_tmp, nch
+    read(6, *, iostat = ios) yr_tmp, mo_tmp, dy_tmp, hh_tmp, mm_tmp, ss_tmp, nch
+    if(ios .ne. 0) then
+      write(0, '(a)') "cannot read waveforms from stdin"
+      error stop
+    endif
     yr(1 : nbuf_sec - 1) = yr(2 : nbuf_sec); yr(nbuf_sec) = yr_tmp
     mo(1 : nbuf_sec - 1) = mo(2 : nbuf_sec); yr(nbuf_sec) = mo_tmp
     dy(1 : nbuf_sec - 1) = dy(2 : nbuf_sec); yr(nbuf_sec) = dy_tmp
