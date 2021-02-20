b1ac376aaa3cbf4227e9eed5498be1fc99e35837
Masashi Ogiso <mogiso@mri-jma.go.jp>
2021-02-20 18:58:46 +0900

    pick up for subevent_amplitude.txt if given in arguments

--- tool/pick_local_maxima.pl
+++ tool/pick_local_maxima.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/env perl
 #
-#pick up local maxima from the result of AmplitudeSourceLocation_masterevent.F90,
-#then write hypocentral distribution
+#pick up local maxima from the result of AmplitudeSourceLocation_(PulseWidth|masterevent).F90,
 
 
 $in = $ARGV[0];
 $out_list = $ARGV[1];
 $sourceamp_min = $ARGV[2];
+$subevent_amplitude = $ARGV[3];
+$subevent_amplitude_out = $ARGV[4];
 
 $lon_w = 136.5;
 $lon_e = 137.5;
@@ -17,10 +18,18 @@ $dep_max = 16.0;
 $residual_max = 0.05;
 
 ##read result file
+$read_comment = 0;
 open IN, "<", $in;
 while(<IN>){
-  next if substr $_, 0, 1 eq "#";
-  chomp $_;
+  chomp;
+  if((substr $_, 0, 1) eq "#"){
+    if($read_comment == 0){
+      $read_comment = 1;
+      $comment1 = $_;
+    }else{
+      next;
+    }
+  }
   push @result_all, $_;
   $_ =~ s/^\s*(.*?)\s*$/$1/;
   @tmp = split /\s+/, $_;
@@ -32,6 +41,23 @@ while(<IN>){
 }
 close IN;
 
+$read_comment = 0;
+if(-f $subevent_amplitude && $subevent_amplitude ne ""){
+  open IN, "<", $subevent_amplitude;
+  while(<IN>){
+    chomp;
+    if((substr $_, 0, 1) eq "#"){
+      if($read_comment == 0){
+        $read_comment = 1;
+        $comment2 = $_;
+      }else{
+        next;
+      }
+    }
+    push @subevent_amp, $_;
+  }
+}
+
 for($i = 1; $i <= $#sourceamp - 1; $i++){
   $diff_back = $sourceamp[$i] - $sourceamp[$i - 1];
   $diff_forward = $sourceamp[$i] - $sourceamp[$i + 1];
@@ -41,14 +67,26 @@ for($i = 1; $i <= $#sourceamp - 1; $i++){
      #$hypo_dep[$i] >= $dep_min && $hypo_dep[$i] <= $dep_max){
      $residual[$i] < $residual_max){
     push @result_localmax_picked, $result_all[$i];
+    if(@subevent_amp){
+      push @subevent_amp_picked, $subevent_amp[$i];
+    }
   }
 }
 
 open OUT, ">", $out_list;
-print OUT "# amp_ratio sigma_ampratio longitude sigma_lon latitude sigma_lat depth sigma_depth ot_tmp\n";
+print OUT "$comment1\n";
 for($i = 0; $i <= $#result_localmax_picked; $i++){
   print OUT "$result_localmax_picked[$i]\n";
 }
 close OUT;
+
+if(@subevent_amp_picked){
+  open OUT, ">", $subevent_amplitude_out;
+  print OUT "$comment2\n";
+  for($i = 0; $i <= $#subevent_amp_picked; $i++){
+    print OUT "$subevent_amp_picked[$i]\n";
+  }
+  close OUT;
+}
 
 
